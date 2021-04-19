01f27f5f0d709980209b4eec67e504cabc647a11
ZanteZyaire <jazamora6@miners.utep.edu>
2021-04-19 03:59:30 -0600

    Created producer/consumer queue with a limit of ten items

---
+++ pc.py
@@ -0,0 +1,20 @@
+
+from threading import Semaphore
+
+class ProductConsumer:
+    def __init__(self):
+        self.queue = []
+        self.isEmpty = Semaphore(0)
+        self.isFull = Semaphore(0)
+    
+    def put(self, item):
+        if len(self.queue) == 10:
+            self.isFull.acquire()
+        self.queue.append(item)
+        self.isEmpty.release()
+    
+    def get(self):
+        self.isEmpty.acquire()
+        item = self.queue.pop(0)
+        self.isFull.release()
+        return item
